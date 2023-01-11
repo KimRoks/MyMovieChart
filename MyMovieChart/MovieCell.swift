b6c89af2a7fc97da90e369e5b953776486d555fb
KimRoks <rudfhr3161@naver.com>
2023-01-11 12:59:04 +0900

    썸네일 이미지 추가
    커스텀 클래스를 이용한 프로토타입 셀 제어

---
+++ MyMovieChart/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MyMovieChart
+//
+//  Created by 김경록 on 2023/01/11.
+//  Copyright © 2023 SQLPRO. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+  
+    @IBOutlet var title: UILabel!
+    @IBOutlet var opendate: UILabel!
+    @IBOutlet var desc: UILabel!
+    @IBOutlet var rating: UILabel!
+    
+    @IBOutlet var thumnail: UIImageView!
+    
+    
+    
+    
+    
+}
